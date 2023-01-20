164a25232b63997e2b1ea0484dc9a7f0cdb66a04
RomilVasoya <118708996+RomilVasoya@users.noreply.github.com>
2023-01-20 10:56:57 +0530

    pr1

---
+++ 3095/1.5.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cout << "enter n ; ";
+	cin >>n;
+	
+	int a[n];
+	
+	for(i=0;i<n;i++)
+	{
+		cout << "a["<< i << "] : ";
+		cin >> a[i];
+}
+	for(i=0;i<n;i++){
+		for(j=1;j<a[i];j++){
+			if(j*j==a[i]){
+				cout <<"a["<< i <<"]:" << j;
+			}
+		}
+		
+	}
+}
---
+++ 3095/pr1.6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i;
+	
+	cout <<"enter n : ";
+	cin >> n;
+	
+	int a[n],b[n];
+	
+	for (i=0;i<n;i++)
+	{
+		cout <<"a["<<i<<"]:";
+		cin>>a[i];
+		b[i]=a[i]*a[i]*a[i];
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		cout <<"b["<<i<<"]:"<<b[i]<<"\n";
+	}
+}
