b5968ec6cc5683730962d6d91460e49be22da022
RomilVasoya <118708996+RomilVasoya@users.noreply.github.com>
2023-02-17 10:26:21 +0530

    6.1
    Signed-off-by: RomilVasoya <118708996+RomilVasoya@users.noreply.github.com>

---
+++ ch6/1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class x{
+	protected:
+		int a,b,c;
+};
+
+class y : public x{
+	public :
+		void setdata(){
+			cout << "enter a :";
+			cin >> a;
+			cout << "enter b :";
+			cin >> b;
+			cout << "enter c :";
+			cin >> c;
+		}
+		void getdata(){
+			cout << "a :" << a << endl;
+			cout << "b :" << b << endl;
+			cout << "c :" << c << endl;
+			
+		}
+};
+
+int main(){
+	 y z;
+	z.setdata();
+	z.getdata();
+}
